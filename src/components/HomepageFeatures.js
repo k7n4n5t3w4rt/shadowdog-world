342a84e9fa912b81be72d96282992979462683e4
Kynan Stewart Hughes <mail@kynan.net.au>
2022-03-01 18:08:11 +1100

    Some copy for the Roadmap.

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -22,15 +22,7 @@ export default function HomepageFeatures() {
                                 plan to release a Shadowdog token. It isn't an
                                 NFT scam. I just wanted to generate a bunch of
                                 tokens because I think it's an interesting kind
-                                of art. There's heaps of them and they will
-                                always be an affordable way to show your
-                                support. Buying the token will keep me drawing
-                                new stories and give you a unique and special
-                                profile pic. There will be much utility. I'm not
-                                sure what yet. I plan to make some merch, so
-                                there's that. Basically anything I do that's
-                                Shadowdog related will probably use the token in
-                                some way. It's the future.
+                                of art.
                             </p>
                         </div>
                     </div>
--- src/pages/roadmap/index.md
+++ src/pages/roadmap/index.md
@@ -2,6 +2,10 @@
 title: Roadmap
 ---
 
-# About Shadowdog
+# Roadmap
 
-What is it all about?
+## 10000 SD PFPs
+
+![Tokens!](/img/sd-pfp.png)
+
+I've been learning about NFTs and plan to release a Shadowdog token. It isn't an NFT scam. I just wanted to generate a bunch of tokens because I think it's an interesting kind of art. There's heaps of them and they will always be an affordable way to show your support. Buying the token will keep me drawing new stories and give you a unique and special profile pic. There will be much utility. I'm not sure what yet. I plan to make some merch, so there's that. Basically anything I do that's Shadowdog related will probably use the token in some way. It's the future.
